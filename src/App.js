e263c27488d6df0efefa88869277d13f4cdc2a7b
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-08-16 22:05:42 +0200

    feat: finish visual integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TimerInterface from './component/timerInterface';
+import start from './images/start.png';
+import reload from './images/reload.png';
+import params from './images/adjust.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="central_bar"></div>
+      <div className="pause icon">
+        <img src={start} className="in-icon" />
+      </div>
+      <div className="restart icon">
+        <img src={reload} className="in-icon" />
+      </div>
+      <div className="params icon">
+        <img src={params} className="in-icon" />
+      </div>
+      <div className="side_top">
+        <TimerInterface />
+      </div>
+      <div className="side_bottom">
+        <TimerInterface />
+      </div>
     </div>
   );
 }
---
+++ src/component/timerInterface.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TimerInterface = () => {
+
+
+    return(
+        <div className="timerInterface">
+            <div className="timerInterface_info">
+                <p>Temps total:</p>
+                <p>Nombre total de tour:</p>
+            </div>
+            <div className="timerInterface_button">
+                120 S
+            </div>
+        </div>
+    )
+}
+
+export default TimerInterface;
